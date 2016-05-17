896031973d1421cd855c0b752a5a15aa0fb6a326
tmdgus0118 <qoohk@u-crm.com>
2016-05-17 16:06:39 +0900

    d

--- index.js
+++ index.js
@@ -2,11 +2,6 @@
 let express = require('express');
 
 let app = express();
-let cors = require('cors');
-app.use((req,res)=>{
-    res.header('Access-Control-Allow-Origin','*.code404.co.kr');
-    res.header('Access-Control-Request-Method','GET, POST, PUT, DELETE')
-});
 app.use(express.static('./pub'));
 
 app.listen(3000);
