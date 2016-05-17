55add61b383009c460582a4174fd53ee68976f65
tmdgus0118 <qoohk@u-crm.com>
2016-05-17 15:50:44 +0900

    cors

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 let express = require('express');
 
 let app = express();
-
+let cors = require('cors');
+app.use(cors({
+    origin : /\.code404\.co\.kr$/ig
+}));
 app.use(express.static('./pub'));
 
 app.listen(3000);
