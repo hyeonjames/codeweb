e5d4f6586adf8bb175a00af3bcb0eb74431b030e
tmdgus0118 <qoohk@u-crm.com>
2016-05-17 10:58:02 +0900

    d

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 'use strict';
-const express = require('express');
 
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const mong = require('mongoose');
-const app = express();
-app.set('view engine','ejs');
+module.exports = (app,express)=>{
+    const session = require('express-session');
+    const bodyParser = require('body-parser');
+    const mong = require('mongoose');
 
-let {Account} = require('./routes/account.js');
-mong.connect('mongodb://localhost/codeweb',function (err){
-    
-});
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+    let {Account} = require('./routes/account.js');
+    mong.connect('mongodb://localhost/codeweb',function (err){
+        
+    });
+    app.use(bodyParser.urlencoded({extended:false}));
+    app.use(bodyParser.json());
 
-app.use(session({
-    secret : 'codeweb',
-    resave : false,
-    saveUninitialized : true
-}))
-app.use(express.static('pub'));
-const routes = {
-    '/api/accounts' : './routes/account.js'
+    app.use(session({
+        secret : 'codeweb',
+        resave : false,
+        saveUninitialized : true
+    }))
+    app.use(express.static('pub'));
+    const routes = {
+        '/api/accounts' : './routes/account.js'
+    }
+
+    for(let route in routes){
+        let url = routes[route];
+        let exports = require(url);
+        let Router = express.Router();
+        exports(Router,app);
+        app.use(route,Router);
+    }
+
+
+//    app.listen(80);
 }
-
-for(let route in routes){
-    let url = routes[route];
-    let exports = require(url);
-    let Router = express.Router();
-    exports(Router,app);
-    app.use(route,Router);
-}
-
-
-app.listen(80);
-
