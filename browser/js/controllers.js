3578a2a33ef9a0e17843b78192d0b94ffc325fb4
tmdgus0118 <qoohk@u-crm.com>
2016-05-18 17:15:33 +0900

    dd

--- browser/js/controllers.js
+++ browser/js/controllers.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
 
         $.extend($s, {
             signin() {
-                location.href = '/signin.html';
+                location.href = '#/sign';
             },
             val: {
                 searchKey: '',
