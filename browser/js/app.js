bdcb9ff423fc16c8ecfa0b294014cc3792bf4d56
tmdgus0118 <qoohk@u-crm.com>
2016-05-18 17:18:32 +0900

    dd

--- browser/js/app.js
+++ browser/js/app.js
@@ -2,41 +2,49 @@ import angular from 'angular';
 import router from 'angular-ui-router';
 import sanitize from 'angular-sanitize';
 
-let app = angular.module('myApp',['ui.router','ngSanitize']);
+let app = angular.module('myApp', ['ui.router', 'ngSanitize']);
 
-app.config(['$stateProvider','$urlRouterProvider',
-($state,$router)=>{
-    $state
-    .state('sign',{
-        url : '/sign',
-        views : {
-            "content" : {
-                templateUrl : 'views/signin.html',
-                controller : 'signCtrl'
-            }
-        }
-    })
-    .state('home',{
-        url : '/home',
-        views : {
-            "content" : {
-                templateUrl : 'views/home.html',
-                controller : 'homeCtrl'
-            }
-        }
-    })
-    .state('welcome',{
-        url : '/welcome',
-        templateUrl : 'views/welcome.html',
-        controller : 'welcomeCtrl'
-    })
-    .state('auth',{
-        url : '/auth/:accNo/:registCode',
-        templateUrl : 'views/auth.html',
-        controller : 'authCtrl'
-    });
-    $router.otherwise('/home');
-}]);
+app.config(['$stateProvider', '$urlRouterProvider',
+    ($state, $router) => {
+        $state
+            .state('sign', {
+                url: '/sign',
+                views: {
+                    "content": {
+                        templateUrl: 'views/signin.html',
+                        controller: 'signCtrl'
+                    }
+                }
+            })
+            .state('home', {
+                url: '/home',
+                views: {
+                    "content": {
+                        templateUrl: 'views/home.html',
+                        controller: 'homeCtrl'
+                    }
+                }
+            })
+            .state('welcome', {
+                url: '/welcome',
+                views: {
+                    "content": {
+                        templateUrl: 'views/welcome.html',
+                        controller: 'welcomeCtrl'
+                    }
+                }
+            })
+            .state('auth', {
+                url: '/auth/:accNo/:registCode',
+                views: {
+                    content: {
+                        templateUrl: 'views/auth.html',
+                        controller: 'authCtrl'
+                    }
+                }
+            });
+        $router.otherwise('/home');
+    }]);
 
 require('./controllers.js')(app);
 require('./directives.js')(app);
